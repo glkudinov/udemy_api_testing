c64d4450e7a586229cad1826e3736a13a9555778
Gleb <monachusgl@gmail.com>
2022-06-21 12:31:55 +0300

    Добавлен метод проверки наличия заданного слова в поле ответа.

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -15,32 +15,39 @@ class TestCreatePlace:
         check_info = post_result.json()
         place_id = check_info.get("place_id")
         Checking.check_json_token(post_result, ['status', 'place_id', 'scope', 'reference', 'id'])
+        Checking.check_json_value(post_result, "status", "OK")
+        Checking.check_json_value(post_result, "scope", "APP")
 
         print("Метод GET -> POST")
         get_result: Response = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(get_result, 200)
         Checking.check_json_token(get_result, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types',
                                                'website', 'language'])
+        Checking.check_json_value(get_result, "address", "29, side layout, cohen 09")
 
         print("Метод PUT")
         put_result: Response = GoogleMapsApi.put_new_place(place_id)
         Checking.check_status_code(put_result, 200)
         Checking.check_json_token(put_result, ['msg'])
-
+        Checking.check_json_value(put_result, "msg", "Address successfully updated")
 
         print("Метод GET -> PUT")
         get_result: Response = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(get_result, 200)
         Checking.check_json_token(get_result, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types',
                                                'website', 'language'])
+        Checking.check_json_value(get_result, "address", "100 Lenina street, RU")
 
         print("Метод DELETE")
         delete_result: Response = GoogleMapsApi.delete_new_place(place_id)
         Checking.check_status_code(delete_result, 200)
         Checking.check_json_token(delete_result, ['status'])
+        Checking.check_json_value(delete_result, "status", "OK")
 
         print("Метод GET -> DELETE")
         get_result: Response = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(get_result, 404)
         Checking.check_json_token(get_result, ['msg'])
+        Checking.check_word_in_json_value(get_result, "failed", "msg")
+
         print("Тестирование создания, изменения и удаления локации завершено успешно")
--- utils/checking.py
+++ utils/checking.py
@@ -10,8 +10,25 @@ class Checking:
         assert status_code == response_status_code, f"Status code must be {status_code}, now {response_status_code}"
         print(f"Request is OK, status code is {response_status_code}")
 
+    """Метод для проверки наличия всех полей в ответе"""
     @staticmethod
     def check_json_token(response: Response, expected_value):
         token = json.loads(response.text)
         assert list(token) == expected_value
         print("Все поля присутствуют")
+
+    """Метод для проверки значения поля в ответе"""
+    @staticmethod
+    def check_json_value(response: Response, field_name: str, expected_value: str):
+        check = response.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value, f"Значение поля {field_name} не соответствует {expected_value}"
+        print(f"Успех. Значение поля {field_name} соответствует {expected_value}")
+
+    """Метод для проверки по слову в ответе"""
+    @staticmethod
+    def check_word_in_json_value(response: Response, word_in_text: str, field_name: str):
+        check = response.json()
+        check_info = check.get(field_name)
+        assert word_in_text in check_info, f"В ответе {check_info} отсутствует слово {word_in_text}"
+        print(f"В ответе {check_info} есть слово {word_in_text}")
